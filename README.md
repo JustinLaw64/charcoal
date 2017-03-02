6cf922ceb52534bcf292676e438031cef37af650
ForbiddenJ <justinscomputercreations@gmail.com>
2017-03-01 18:33:02 -0600

    Initial upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Charcoal
+
+Allows creation of coal through cooking trees.
+
+Cook a tree block to get a coal lump.
+
+## License: Unlicense
---
+++ init.lua
@@ -0,0 +1,8 @@
+-- Charcoal mod
+
+minetest.register_craft({
+    type = "cooking",
+    recipe = "group:tree",
+    output = "default:coal_lump",
+    cooktime = 3.5,
+})
